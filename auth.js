a1bb5d4cc1b5bd4a9592b5482f31850555d2507c
strawberry-p <strawberry@example.com>
2025-10-26 22:42:27 +0100

    Fixed links on login/register pages, renamed pages to make more sense

--- auth.js
+++ auth.js
@@ -19,8 +19,8 @@ function kick_url(){
     let wl = window.location //"in" doesn't work in JS apparently
     if (wl.protocol.includes("file")) {return "file"} //ts local testing is pmo.
     if (authed && (url.includes("login") || url.includes("register"))) {
-        console.log("redirect to "+wl.protocol+wl.hostname+"/story.html")
-        window.location.assign(wl.protocol+wl.hostname+"/story.html") //redirect to the campaigns page to be actually useful
+        console.log("redirect to "+wl.protocol+wl.hostname+"/campaigns.html")
+        window.location.assign(wl.protocol+wl.hostname+"/campaigns.html") //redirect to the campaigns page to be actually useful
 
     }
     else if (!authed && !(url.includes("login") || url.includes("register"))) {
