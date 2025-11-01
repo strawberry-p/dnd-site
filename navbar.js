683435f79241b409c43f85a0724f8e9e3977fa19
strawberry-p <strawberry@example.com>
2025-11-01 23:33:45 +0100

    added href injection to navbar links

--- navbar.js
+++ navbar.js
@@ -9,5 +9,11 @@ async function inject_navbar() {
     const navCharacters = document.getElementById("nav-characters")
     const navLogin = document.getElementById("nav-login")
     const navMain = document.getElementById("nav-homepage")
+    const navCurrent = document.getElementById(currentPage) //defined in script tags on the page
+    console.log(navCurrent)
+    navCampaigns.href = rootURL+'campaigns.html'
+    navCharacters.href = rootURL+'characters.html'
+    navLogin.href = rootURL+'login.html'
+    navCurrent.className = navCurrent.className+" current"
 }
 inject_navbar()
